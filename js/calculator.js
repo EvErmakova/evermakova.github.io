d0fc77356dfb8dfc20f2220dab0e8bcdc7d7240b
e.russkih <e.russkih@artsofte.ru>
2019-11-08 14:12:58 +0300

    add point and fix bugs

--- js/calculator.js
+++ js/calculator.js
@@ -3,44 +3,102 @@
     var btnClass = '.calculator__btn';
     var numberClass = '.calculator__btn--number';
     var calcWindow = calc.querySelector('.calculator__window');
-    var btnsID = { sum: '#sumBtn', clear: '#clearBtn', backspace: '#backspaceBtn'};
+    var btnID = {
+        sum: '#sumBtn',
+        clear: '#clearBtn',
+        backspace: '#backspaceBtn',
+        point: '#pointDtn',
+        multiply: '#multiplyBtn',
+        plus: '#plusBtn',
+        point: '#pointBtn'
+    };
+    var keyCode = {
+        'one': 49,
+        'two': 50,
+        'three': 51,
+        'four': 52,
+        'five': 53,
+        'six': 54,
+        'seven': 55,
+        'eight': 56,
+        'nine': 57,
+        'zero': 48,
+        'clear': 27,
+        'backspace': 8,
+        'division': 191,
+        'minus': 189,
+        'sum': 187,
+        'enter': 13,
+        'point': 190
+    };
     var string = '0';
 
     //Начальное значение
     calcWindow.innerHTML = '0';
 
     calc.addEventListener('click', function (evt) {
-        if (evt.target.closest(btnsID.clear)) {
-            // Если нажали очистить
-            string = '0';
-        } else if (evt.target.closest(btnsID.backspace)) {
-            // Если нажали удалить последний символ
-            if (string.length > 1) {
-                string = string.substring(0, string.length - 1);
-            } else {
-                string = '0';
-            }
-        } else if (evt.target.closest(btnsID.sum)) {
-            // Если нажали равно
-            if (string.slice(-1) !== '+' && string.slice(-1) !== '-' && string.slice(-1) !== '/' && string.slice(-1) !== '*') {
-                string = eval(string).toString();
-            }
-        } else if (evt.target.closest(btnClass) && !evt.target.disabled) {
-            // Если нажали кнопку
+        if (evt.target.closest(btnClass) && !evt.target.disabled) {
             var btnValue = evt.target.closest(btnClass).value;
-            if (string == '0' && !evt.target.closest(numberClass) && btnValue !== '-') {
-            } else if ((string.slice(-1) === '+' || string.slice(-1) === '-' || string.slice(-1) === '/' || string.slice(-1) === '*') && !evt.target.closest(numberClass)) {
-                if (btnValue !== string.slice(-1)) {
-                    string = string.substring(0, string.length - 1) + btnValue;
-                }
-            } else {
-                if (string == '0') {
-                    string = btnValue;
+            if (evt.target.closest(btnID.clear)) {
+                // Если нажали очистить
+                string = '0';
+            } else if (evt.target.closest(btnID.backspace)) {
+                // Если нажали удалить последний символ
+                if (string.length > 1) {
+                    string = string.substring(0, string.length - 1);
                 } else {
+                    string = '0';
+                }
+            } else if (evt.target.closest(btnID.sum)) {
+                // Если нажали равно
+                if (string.slice(-1) !== '+' && string.slice(-1) !== '-' && string.slice(-1) !== '/' && string.slice(-1) !== '*') {
+                    string = eval(string).toString();
+                }
+            } else if (evt.target.closest(btnID.point)) {
+                // Если нажали точку
+                var lastNumber = '';
+                for (var i = string.length - 1; i >= 0; i --) {
+                    if (isNaN(string[i])) {
+                        lastNumber = string.slice(i);
+                        break;
+                    }
+                }
+                if (!lastNumber.includes('.')) {
                     string += btnValue;
                 }
+            } else {
+                // Если нажали любую другую кнопку
+                if (string == '0' && !evt.target.closest(numberClass) && btnValue !== '-') {
+                } else if ((string.slice(-1) === '+' || string.slice(-1) === '-' || string.slice(-1) === '/' || string.slice(-1) === '*') && !evt.target.closest(numberClass)) {
+                    if (btnValue !== string.slice(-1)) {
+                        string = string.substring(0, string.length - 1) + btnValue;
+                    }
+                } else {
+                    if (string == '0') {
+                        string = btnValue;
+                    } else {
+                        string += btnValue;
+                    }
+                }
             }
+            calcWindow.innerHTML = string;
         }
-        calcWindow.innerHTML = string;
+    });
+
+    window.addEventListener('keydown', function (evt) {
+        var key = evt.keyCode;
+        Object.keys(keyCode).forEach(function (item) {
+            if (key == keyCode['enter']) {
+                document.querySelector(btnID.sum).click();
+            } else if (evt.shiftKey) {
+                if (key == keyCode['eight']) {
+                    document.querySelector(btnID.multiply).click();
+                } else if (key == keyCode['sum']) {
+                    document.querySelector(btnID.plus).click();
+                }
+            } else if (key == keyCode[item]) {
+                document.querySelector('#' + item + 'Btn').click();
+            }
+        });
     });
 })();
